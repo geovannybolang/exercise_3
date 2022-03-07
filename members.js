75b20c2be2d28d5d3325acee6857654984e785a5
Andersonsumaga123 <s21910455@student.unklab.ac.id>
2022-03-07 21:40:55 +0800

    Menambahkan index user.js dan member.js

---
+++ members.js
@@ -0,0 +1,26 @@
+module.exports.data = [{
+    id: 1,
+    nim: 105021910052,
+    nama: "Geovany Bolang",
+    noTelp: 081288850995
+}, {
+    id: 2,
+    nim: 105021910009,
+    nama: "Kanter, Jeri Leri",
+    noTelp: 08895395497617
+}, {
+    id: 3,
+    nim: 105021910066,
+    nama: "Sumaga, Anderson Valentino",
+    noTelp: 085242703036
+}, {
+    id: 4,
+    nim: 105021910005,
+    nama: "Sangari Richard Ericsen",
+    noTelp: 082394649029
+}, {
+    id: 5,
+    nim: 105021910028,
+    nama: "Rainer Cristian",
+    noTelp: 082271142575
+}];
